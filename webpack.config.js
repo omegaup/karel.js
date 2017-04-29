9c0094c262a0e82e3a5d740f270f429a49075e6a
lhchavez <lhchavez@lhchavez.com>
2017-04-29 05:04:23 +0000

    Generando configuración de webpack para minijail
    Es bastante más sencillo de hacer deployment si hay que modificar un
    solo archivo en vez de tener que andar cargando montones de módulos de
    npm. Este cambio agrega `npm run build:bundle` para construir
    `dist/karel.js`, que es un solo archivo ejecutable listo para mandar al
    runner.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry : './js/cli.js',
+  module : {
+    rules : [
+      {test : /\.js$/, use : 'shebang-loader'},
+    ],
+  },
+  output : {
+    path : path.join(__dirname, '/dist/'),
+    filename : 'karel.js',
+    publicPath : './',
+    sourceMapFilename : 'karel.map'
+  },
+  target : 'node',
+  plugins : [
+    new webpack.BannerPlugin(
+        {banner : '#!/usr/bin/env node', raw : true, entryOnly : true}),
+    new webpack.LoaderOptionsPlugin({
+      minimize : true,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap : false,
+    }),
+  ]
+};
